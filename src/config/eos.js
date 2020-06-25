01300354752f4b22abc7a923951071445a6c1636
Adriel Diaz <adriexnet@gmail.com>
2020-06-25 14:39:04 -0600

    chore: allow filter nodes

--- src/config/eos.js
+++ src/config/eos.js
@@ -2,3 +2,25 @@ export const endpoint = process.env.REACT_APP_EOS_ENDPOINT
 export const exchangeRate = process.env.REACT_APP_EOS_DEFAULT_EXCHANGE_RATE
 export const exchangeRateApi =
   process.env.REACT_APP_EOS_DEFAULT_EXCHANGE_RATE_API
+export const nodeTypes = [
+  {
+    name: 'producer',
+    color: 'blue',
+    description: 'Node with signing key'
+  },
+  {
+    name: 'full',
+    color: 'green',
+    description: 'Node in front of producer'
+  },
+  {
+    name: 'query',
+    color: 'yellow',
+    description: 'Node that provides HTTP(S) API to the public'
+  },
+  {
+    name: 'seed',
+    color: 'red',
+    description: 'Node that provides P2P and/or BNET to the public'
+  }
+]
