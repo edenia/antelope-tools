9dbae60faca73c2685efa4f39730dff2e7aaca29
Andres Gomez <kurono@riseup.net>
2020-08-27 20:32:57 +0000

    add build for testing and lacchain.
    refs #115

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,13 +8,19 @@ fi
 
 echo "Building docker containers..."
 if [ "$1" == "production" ]; then
+    cp ./.env.mainnet ./.env
     docker build -t eoscostarica506/monitor-webapp:latest --target run-stage webapp/
     docker push eoscostarica506/monitor-webapp:latest
     docker build -t eoscostarica506/monitor-hapi hapi/
     docker push eoscostarica506/monitor-hapi
-    docker build -t eoscostarica506/wallet wallet/
+    docker build -t eoscostarica506/wallet --target run-stage wallet/
     docker push eoscostarica506/wallet
 elif [ "$1" == "testing" ]; then
+    cp ./.env.jungle ./.env
     docker build -t eoscostarica506/monitor-webapp:testing webapp/
-    docker push eoscostarica506/monitor-webapp:testing hapi/
+    docker push eoscostarica506/monitor-webapp:testing
+elif [ "$1" == "lacchain" ]; then
+    cp ./.env.lacchain ./.env
+    docker build -t eoscostarica506/monitor-webapp:lacchain webapp/
+    docker push eoscostarica506/monitor-webapp:lacchain
 fi
