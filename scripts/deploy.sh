a975be1c54cd49da9cc6b6f965822678850da57c
Andres Gomez <kurono@riseup.net>
2020-09-03 13:47:26 +0000

    improve deploy script.
    refs #115.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,7 @@ fi
 
 if [ "$1" == "production" ]; then
     kubectl create -f kubernetes/ --namespace="$1"
+    kubectl create -f kubernetes/production --namespace="$1"
     kubectl expose deployment monitor-webapp --name=monitor-port-ssl --port=443 \
             --target-port=443 --type=ClusterIP --external-ip="$2" -n "$1"
     kubectl expose deployment monitor-webapp --name=monitor-port --port=80 \
@@ -16,7 +17,8 @@ if [ "$1" == "production" ]; then
             --target-port=8080  --type=ClusterIP --external-ip="$2" -n "$1"
 elif [ "$1" == "testing" ]; then
     kubectl create -f kubernetes/ --namespace="$1"
-    kubectl expose deployment monitor-webapp --name=monitor-port --port=80 \
+    kubectl create -f kubernetes/testing --namespace="$1"
+    kubectl expose deployment monitor-webapp --name=monitor-port --port=3000 \
             --target-port=3000  --type=ClusterIP --external-ip="$2" -n "$1"
     kubectl expose deployment monitor-hasura --name=hasura-port --port=8080 \
             --target-port=8080  --type=ClusterIP --external-ip="$2" -n "$1"
