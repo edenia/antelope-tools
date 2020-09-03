8257dcd16997ec0117df338553451605922c84a8
Andres Gomez <kurono@riseup.net>
2020-09-03 19:39:22 +0000

    improve the deployment script. Do not expose the graphql pots.
    refs #115

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,13 +13,9 @@ if [ "$1" == "production" ]; then
             --target-port=443 --type=ClusterIP --external-ip="$2" -n "$1"
     kubectl expose deployment monitor-webapp --name=monitor-port --port=80 \
             --target-port=80  --type=ClusterIP --external-ip="$2" -n "$1"
-    kubectl expose deployment monitor-hasura --name=hasura-port --port=8080 \
-            --target-port=8080  --type=ClusterIP --external-ip="$2" -n "$1"
 elif [ "$1" == "testing" ]; then
     kubectl create -f kubernetes/ --namespace="$1"
     kubectl create -f kubernetes/testing --namespace="$1"
     kubectl expose deployment monitor-webapp --name=monitor-port --port=3000 \
             --target-port=3000  --type=ClusterIP --external-ip="$2" -n "$1"
-    kubectl expose deployment monitor-hasura --name=hasura-port --port=8080 \
-            --target-port=8080  --type=ClusterIP --external-ip="$2" -n "$1"
 fi
