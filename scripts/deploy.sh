3efeb58fa883abe25eca855088842712a02bfa79
Andres Gomez <kurono@riseup.net>
2020-09-29 15:46:28 +0000

    creates the lacchain deployment.
    refs #141

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,7 +20,9 @@ elif [ "$1" == "testing" ]; then
             --target-port=3000  --type=ClusterIP --external-ip="$2" -n "$1"
 elif [ "$1" == "lacchain" ]; then
     kubectl create -f kubernetes/ --namespace="$1"
-    kubectl create -f kubernetes/testing --namespace="$1"
-    kubectl expose deployment monitor-webapp --name=monitor-port --port=8000 \
-            --target-port=3000  --type=ClusterIP --external-ip="$2" -n "$1"
+    kubectl create -f kubernetes/lacchain --namespace="$1"
+    kubectl expose deployment monitor-webapp --name=monitor-port-ssl --port=443 \
+            --target-port=443 --type=ClusterIP --external-ip="$2" -n "$1"
+    kubectl expose deployment monitor-webapp --name=monitor-port --port=80 \
+            --target-port=80  --type=ClusterIP --external-ip="$2" -n "$1"
 fi
