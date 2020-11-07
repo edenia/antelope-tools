b4dd1955888cc6a81c5f143b2cfc3d52ffd823cd
Adriel Diaz <adriexnet@gmail.com>
2020-11-07 17:10:02 -0600

    feat: add node list to entities/producers card

--- webapp/src/components/ProducerCard.js
+++ webapp/src/components/ProducerCard.js
@@ -18,6 +18,7 @@ import CountryFlag from './CountryFlag'
 import ProducerHealthIndicators from './ProducerHealthIndicators'
 import UsageChart from './UsageChart'
 import ProducerSocialLinks from './ProducerSocialLinks'
+import { Typography } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,203 +50,330 @@ const useStyles = makeStyles((theme) => ({
     '& svg': {
       marginRight: theme.spacing(1)
     }
+  },
+  action: {
+    cursor: 'pointer',
+    color: theme.palette.secondary.main,
+    '&:hover': {
+      textDecoration: 'underline'
+    }
   }
 }))
 
-const ProducerCard = ({ producer, rank }) => {
+const ProducerCard = ({ producer, onNodeClick, rank }) => {
   const classes = useStyles()
   const { t } = useTranslation('producerSummary')
+
+  const Avatar = () => {
+    return (
+      <img
+        className={classes.avatar}
+        src={
+          producer.bp_json?.branding?.logo_256 ||
+          generalConfig.defaultProducerLogo
+        }
+        onError={onImgError(generalConfig.defaultProducerLogo)}
+        alt="avatar"
+      />
+    )
+  }
+  const SubHeader = () => {
+    return (
+      <>
+        <CountryFlag code={producer.bp_json?.location?.country} />
+        <span className={classes.country}>
+          {producer.bp_json?.location?.name || 'N/A'}
+        </span>
+      </>
+    )
+  }
+  const Rank = () => {
+    return (
+      <>
+        {generalConfig.useVotes && (
+          <>
+            <dt className={classes.dt}>{t('rank')}:</dt>
+            <dd>#{rank}</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const BusinessContact = () => {
+    return (
+      <>
+        {producer.bp_json?.business_contact && (
+          <>
+            <dt className={classes.dt}>{t('businessContact')}:</dt>
+            <dd>{producer.bp_json.business_contact}</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const TechnicalContact = () => {
+    return (
+      <>
+        {producer.bp_json?.technical_contact && (
+          <>
+            <dt className={classes.dt}>{t('technicalContact')}:</dt>
+            <dd>{producer.bp_json.technical_contact}</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const Email = () => {
+    return (
+      <>
+        {producer.bp_json?.email && (
+          <>
+            <dt className={classes.dt}>{t('email')}:</dt>
+            <dd>
+              <Link
+                href={`mailto:${producer.bp_json.email}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {producer.bp_json.email}
+              </Link>
+            </dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const Website = () => {
+    return (
+      <>
+        {producer.bp_json?.website && (
+          <>
+            <dt className={classes.dt}>{t('website')}:</dt>
+            <dd>
+              <Link
+                href={producer.bp_json.website}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {producer.bp_json.website}
+              </Link>
+            </dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const Social = () => {
+    return (
+      <>
+        {producer.bp_json?.social && (
+          <>
+            <dt className={classes.dt}>{t('social')}:</dt>
+            <dd className={classes.social}>
+              <ProducerSocialLinks items={producer.bp_json.social} />
+            </dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const OwnershipDisclosure = () => {
+    return (
+      <>
+        {producer.bp_json?.ownership_disclosure && (
+          <>
+            <dt className={classes.dt}>{t('ownershipDisclosure')}:</dt>
+            <dd>
+              <Link
+                href={producer.bp_json.ownership_disclosure}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {producer.bp_json.ownership_disclosure}
+              </Link>
+            </dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const ChainResources = () => {
+    return (
+      <>
+        {producer.bp_json?.chain_resources && (
+          <>
+            <dt className={classes.dt}>{t('chainResources')}:</dt>
+            <dd>
+              <Link
+                href={producer.bp_json?.chain_resources}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {producer.bp_json?.chain_resources}
+              </Link>
+            </dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const OtherResources = () => {
+    return (
+      <>
+        {producer.bp_json?.other_resources?.length > 0 && (
+          <>
+            <dt className={classes.dt}>{t('otherResources')}:</dt>
+            {producer.bp_json.other_resources.map((url, i) => (
+              <dd key={i}>
+                <Link href={url} target="_blank" rel="noopener noreferrer">
+                  {url}
+                </Link>
+              </dd>
+            ))}
+          </>
+        )}
+      </>
+    )
+  }
+  const ServerVersion = () => {
+    return (
+      <>
+        {producer.server_version_string && (
+          <>
+            <dt className={classes.dt}>{t('serverVersion')}:</dt>
+            <dd>{producer.server_version_string}</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const Ping = () => {
+    return (
+      <>
+        {producer.ping && (
+          <>
+            <dt className={classes.dt}>{t('pingFromCostaRica')}:</dt>
+            <dd>{producer.ping}ms</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const Votes = () => {
+    return (
+      <>
+        {generalConfig.useVotes && (
+          <>
+            <dt className={classes.dt}>{t('votes')}:</dt>
+            <dd>{formatWithThousandSeparator(producer.total_votes_eos, 2)}</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const Rewards = () => {
+    return (
+      <>
+        {generalConfig.useRewards && (
+          <>
+            <dt className={classes.dt}>{t('rewards')}:</dt>
+            <dd>{formatWithThousandSeparator(producer.total_rewards, 2)}</dd>
+          </>
+        )}
+      </>
+    )
+  }
+  const MissedBlocks = () => {
+    return (
+      <>
+        <dt className={classes.dt}>{t('missedBlocks')}</dt>
+        <dd>
+          {(producer.missed_blocks || []).reduce(
+            (result, current) => result + current.value,
+            0
+          )}
+        </dd>
+      </>
+    )
+  }
+  const LastTimeChecked = () => {
+    return (
+      <>
+        <dt className={classes.dt}>{t('lastTimeChecked')}</dt>
+        <dd>
+          {moment(new Date()).diff(moment(producer.updated_at), 'seconds')}
+          {t('secondsAgo')}
+        </dd>
+      </>
+    )
+  }
+  const HealthStatus = () => {
+    return (
+      <>
+        <dt className={classes.dt}>{t('healthStatus')}</dt>
+        <dd>
+          <ProducerHealthIndicators producer={producer} />
+        </dd>
+      </>
+    )
+  }
+  const CpuBenchmark = () => {
+    return (
+      <>
+        {producer.cpus?.length > 0 && (
+          <>
+            <dt className={classes.dt}>{t('cpuUsage')}</dt>
+            <dd>
+              <UsageChart items={producer.cpus} />
+            </dd>
+          </>
+        )}
+      </>
+    )
+  }
 
   return (
     <Card className={classes.root}>
       <CardHeader
-        avatar={
-          <img
-            className={classes.avatar}
-            src={
-              producer.bp_json?.branding?.logo_256 ||
-              generalConfig.defaultProducerLogo
-            }
-            onError={onImgError(generalConfig.defaultProducerLogo)}
-            alt="avatar"
-          />
-        }
+        avatar={<Avatar />}
         title={
           producer.bp_json?.candidate_name ||
           producer.bp_json?.organization_name ||
           producer.owner
         }
-        subheader={
-          <>
-            <CountryFlag code={producer.bp_json?.location?.country} />
-            <span className={classes.country}>
-              {producer.bp_json?.location?.name || 'N/A'}
-            </span>
-          </>
-        }
+        subheader={<SubHeader />}
       />
       <CardContent className={classes.content}>
         <dl className={classes.dl}>
-          {generalConfig.useVotes && (
+          <Rank />
+          <BusinessContact />
+          <TechnicalContact />
+          <Email />
+          <Website />
+          <Social />
+          <OwnershipDisclosure />
+          <ChainResources />
+          <OtherResources />
+          {producer.bp_json?.nodes?.length > 0 && (
             <>
-              <dt className={classes.dt}>{t('rank')}:</dt>
-              <dd>#{rank}</dd>
-            </>
-          )}
-
-          {producer.bp_json?.business_contact && (
-            <>
-              <dt className={classes.dt}>{t('businessContact')}:</dt>
-              <dd>{producer.bp_json.business_contact}</dd>
-            </>
-          )}
-
-          {producer.bp_json?.technical_contact && (
-            <>
-              <dt className={classes.dt}>{t('technicalContact')}:</dt>
-              <dd>{producer.bp_json.technical_contact}</dd>
-            </>
-          )}
-
-          {producer.bp_json?.email && (
-            <>
-              <dt className={classes.dt}>{t('email')}:</dt>
-              <dd>
-                <Link
-                  href={`mailto:${producer.bp_json.email}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {producer.bp_json.email}
-                </Link>
-              </dd>
-            </>
-          )}
-
-          {producer.bp_json?.website && (
-            <>
-              <dt className={classes.dt}>{t('website')}:</dt>
-              <dd>
-                <Link
-                  href={producer.bp_json.website}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {producer.bp_json.website}
-                </Link>
-              </dd>
-            </>
-          )}
-
-          {producer.bp_json?.social && (
-            <>
-              <dt className={classes.dt}>{t('social')}:</dt>
-              <dd className={classes.social}>
-                <ProducerSocialLinks items={producer.bp_json.social} />
-              </dd>
-            </>
-          )}
-
-          {producer.bp_json?.ownership_disclosure && (
-            <>
-              <dt className={classes.dt}>{t('ownershipDisclosure')}:</dt>
-              <dd>
-                <Link
-                  href={producer.bp_json.ownership_disclosure}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {producer.bp_json.ownership_disclosure}
-                </Link>
-              </dd>
-            </>
-          )}
-
-          {producer.bp_json?.chain_resources && (
-            <>
-              <dt className={classes.dt}>{t('chainResources')}:</dt>
-              <dd>
-                <Link
-                  href={producer.bp_json?.chain_resources}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {producer.bp_json?.chain_resources}
-                </Link>
-              </dd>
-            </>
-          )}
-
-          {producer.bp_json?.other_resources?.length > 0 && (
-            <>
-              <dt className={classes.dt}>{t('otherResources')}:</dt>
-              {producer.bp_json.other_resources.map((url, i) => (
-                <dd key={i}>
-                  <Link href={url} target="_blank" rel="noopener noreferrer">
-                    {url}
-                  </Link>
+              <dt className={classes.dt}>{t('nodes')}:</dt>
+              {producer.bp_json.nodes.map((node, i) => (
+                <dd className={classes.action} key={`node-${i}`}>
+                  <Typography onClick={onNodeClick({ node, producer })}>
+                    {node.node_name || node.node_type}
+                  </Typography>
                 </dd>
               ))}
             </>
           )}
-
-          {producer.server_version_string && (
-            <>
-              <dt className={classes.dt}>{t('serverVersion')}:</dt>
-              <dd>{producer.server_version_string}</dd>
-            </>
-          )}
-
-          {producer.ping && (
-            <>
-              <dt className={classes.dt}>{t('pingFromCostaRica')}:</dt>
-              <dd>{producer.ping}ms</dd>
-            </>
-          )}
-
-          {generalConfig.useVotes && (
-            <>
-              <dt className={classes.dt}>{t('votes')}:</dt>
-              <dd>
-                {formatWithThousandSeparator(producer.total_votes_eos, 2)}
-              </dd>
-            </>
-          )}
-
-          {generalConfig.useRewards && (
-            <>
-              <dt className={classes.dt}>{t('rewards')}:</dt>
-              <dd>{formatWithThousandSeparator(producer.total_rewards, 2)}</dd>
-            </>
-          )}
-
-          <dt className={classes.dt}>{t('missedBlocks')}</dt>
-          <dd>
-            {(producer.missed_blocks || []).reduce(
-              (result, current) => result + current.value,
-              0
-            )}
-          </dd>
-
-          <dt className={classes.dt}>{t('lastTimeChecked')}</dt>
-          <dd>
-            {moment(new Date()).diff(moment(producer.updated_at), 'seconds')}
-            {t('secondsAgo')}
-          </dd>
-
-          <dt className={classes.dt}>{t('healthStatus')}</dt>
-          <dd>
-            <ProducerHealthIndicators producer={producer} />
-          </dd>
-
-          {producer.cpus?.length > 0 && (
-            <>
-              <dt className={classes.dt}>{t('cpuUsage')}</dt>
-              <dd>
-                <UsageChart items={producer.cpus} />
-              </dd>
-            </>
-          )}
+          <ServerVersion />
+          <Ping />
+          <Votes />
+          <Rewards />
+          <MissedBlocks />
+          <LastTimeChecked />
+          <HealthStatus />
+          <CpuBenchmark />
         </dl>
       </CardContent>
       <CardActions disableSpacing />
---
+++ webapp/src/routes/NodesDistribution.js
@@ -0,0 +1,215 @@
+/* eslint camelcase: 0 */
+import React, { useEffect, useState, lazy } from 'react'
+import clsx from 'clsx'
+import { makeStyles } from '@material-ui/styles'
+import { useTranslation } from 'react-i18next'
+import { useQuery } from '@apollo/react-hooks'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Box from '@material-ui/core/Box'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  ZoomableGroup
+} from 'react-simple-maps'
+import { geoTimes } from 'd3-geo-projection'
+import { geoPath } from 'd3-geo'
+
+import { eosConfig } from '../config'
+import { NODES_QUERY } from '../gql'
+import PageTitle from '../components/PageTitle'
+import Tooltip from '../components/Tooltip'
+import NodeCard from '../components/NodeCard'
+
+const NodeSearch = lazy(() => import('../components/NodeSearch'))
+
+const defaultScale = 170
+const maxZoom = 3
+const projection = geoTimes()
+const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
+
+const useStyles = makeStyles((theme) => ({
+  mapWrapper: {
+    width: '100%'
+  },
+  geography: {
+    outline: 'none',
+    cursor: 'zoom-in'
+  },
+  geographyZoomOut: {
+    cursor: 'zoom-out'
+  },
+  marker: {
+    cursor: 'pointer'
+  }
+}))
+
+const Nodes = () => {
+  const { data: { loading, producer: producers } = {} } = useQuery(NODES_QUERY)
+  const [nodes, setNodes] = useState([])
+  const [current, setCurrent] = useState(null)
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [filters, setFilters] = useState({ producer: 'all', nodeType: 'all' })
+  const [allNodes, setAllNodes] = useState([])
+  const classes = useStyles()
+  const { t } = useTranslation('dashboardNodes')
+  const [mapState, setMapState] = useState({
+    scale: defaultScale,
+    center: [0, 0],
+    zoom: 1
+  })
+
+  const handlePopoverOpen = (node) => (event) => {
+    setCurrent(node)
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null)
+  }
+
+  const toogleZoom = (geography) => {
+    if (mapState.zoom === maxZoom) {
+      setMapState((state) => ({
+        ...state,
+        center: [0, 0],
+        zoom: 1,
+        scale: defaultScale
+      }))
+
+      return
+    }
+
+    const path = geoPath().projection(projection)
+    const center = projection.invert(path.centroid(geography))
+
+    setMapState((state) => ({
+      ...state,
+      center,
+      zoom: maxZoom,
+      scale: defaultScale * maxZoom
+    }))
+  }
+
+  useEffect(() => {
+    if (!producers?.length) {
+      return
+    }
+
+    const items = []
+    producers.forEach((producer) => {
+      if (!producer?.bp_json?.nodes) {
+        return
+      }
+
+      producer.bp_json.nodes.forEach((node) => {
+        if (!node?.location?.latitude || !node?.location?.longitude) {
+          return
+        }
+
+        items.push({
+          node,
+          producer,
+          coordinates: [node.location.longitude, node.location.latitude]
+        })
+      })
+    })
+
+    setAllNodes(items)
+  }, [producers])
+
+  useEffect(() => {
+    let items = allNodes
+
+    if (filters.nodeType !== 'all') {
+      items = items.filter(
+        (current) => current.node.node_type === filters.nodeType
+      )
+    }
+
+    if (filters.producer !== 'all') {
+      items = items.filter((current) => current.node.owner === filters.producer)
+    }
+
+    setNodes(items)
+  }, [allNodes, filters])
+
+  return (
+    <Box>
+      <PageTitle title={t('htmlTitle')} />
+      <NodeSearch
+        producers={producers}
+        filters={filters}
+        onChange={setFilters}
+      />
+      {loading && <LinearProgress />}
+      {!loading && (
+        <ComposableMap
+          className={classes.map}
+          projectionConfig={{
+            scale: mapState.scale
+          }}
+        >
+          <ZoomableGroup center={mapState.center} maxZoom={1}>
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography
+                    onClick={() => toogleZoom(geo)}
+                    className={clsx({
+                      [classes.geography]: true,
+                      [classes.geographyZoomOut]: mapState.zoom === maxZoom
+                    })}
+                    key={geo.rsmKey}
+                    geography={geo}
+                    stroke="#8F9DA4"
+                    fill="#EEEEEE"
+                  />
+                ))
+              }
+            </Geographies>
+            {nodes.map(({ coordinates, ...current }, i) => (
+              <Marker
+                key={`marker-${i}`}
+                coordinates={coordinates}
+                className={classes.marker}
+                onClick={handlePopoverOpen(current)}
+              >
+                <g
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  transform="translate(-12, -24)"
+                >
+                  <path
+                    d="M12 21.7 C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 12z"
+                    fill={
+                      (
+                        eosConfig.nodeTypes.find(
+                          (noteType) => noteType.name === current.node.node_type
+                        ) || {}
+                      ).color || '#f58a42'
+                    }
+                  />
+                  <circle cx="12" cy="10" r="3" fill="white" />
+                </g>
+              </Marker>
+            ))}
+          </ZoomableGroup>
+        </ComposableMap>
+      )}
+      <Tooltip
+        anchorEl={anchorEl}
+        open={anchorEl !== null}
+        onClose={handlePopoverClose}
+      >
+        <NodeCard node={current?.node} producer={current?.producer} />
+      </Tooltip>
+    </Box>
+  )
+}
+
+Nodes.propTypes = {}
+
+export default Nodes
