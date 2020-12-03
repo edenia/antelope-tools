aa7580271d146997e2db64ce7fe5543dba4ec8c8
Andres Gomez <kurono@riseup.net>
2020-12-03 11:59:21 -0600

    feat: unify webapp deployment files and update deploy script. refs #190

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,21 +6,18 @@ if [ $# -eq 0 ]
     exit 0
 fi
 
+make build-kubernetes
+make deploy-kubernetes
+
 if [ "$1" == "production" ]; then
-    kubectl create -f kubernetes/ --namespace="$1"
-    kubectl create -f kubernetes/production --namespace="$1"
     kubectl expose deployment monitor-webapp --name=monitor-port-ssl --port=443 \
             --target-port=443 --type=ClusterIP --external-ip="$2" -n "$1"
     kubectl expose deployment monitor-webapp --name=monitor-port --port=80 \
             --target-port=80  --type=ClusterIP --external-ip="$2" -n "$1"
 elif [ "$1" == "testing" ]; then
-    kubectl create -f kubernetes/ --namespace="$1"
-    kubectl create -f kubernetes/testing --namespace="$1"
     kubectl expose deployment monitor-webapp --name=monitor-port --port=3000 \
             --target-port=3000  --type=ClusterIP --external-ip="$2" -n "$1"
 elif [ "$1" == "lacchain" ]; then
-    kubectl create -f kubernetes/ --namespace="$1"
-    kubectl create -f kubernetes/lacchain --namespace="$1"
     kubectl expose deployment monitor-webapp --name=monitor-port-ssl --port=443 \
             --target-port=443 --type=ClusterIP --external-ip="$2" -n "$1"
     kubectl expose deployment monitor-webapp --name=monitor-port --port=80 \
