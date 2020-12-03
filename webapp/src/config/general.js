6dcd28efa4ab5bfd79cda7204f18d6b5e67e9db7
Adriel Diaz <adriexnet@gmail.com>
2020-12-03 14:58:34 -0600

    chore: add app version and name

--- webapp/src/config/general.js
+++ webapp/src/config/general.js
@@ -5,3 +5,5 @@ export const eosRateLink = process.env.REACT_APP_EOS_RATE_LINK
 export const defaultProducerLogo =
   process.env.REACT_APP_DEFAULT_PRODUCER_LOGO ||
   'https://bloks.io/img/eosio.png'
+export const appVersion = process.env.REACT_APP_VERSION || 'v1.0'
+export const appName = process.env.REACT_APP_NAME || 'eosiodasboard'
