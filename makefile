820a8ccc72ee39f4cdc215ae0c85b9ad881b08d1
Andres Gomez <kurono@riseup.net>
2020-12-22 15:54:51 -0600

    use the docker.io registry until we solve the github registry error. refs #318.

--- makefile
+++ makefile
@@ -116,7 +116,7 @@ build-docker-images:
 
 push-docker-images: ##@devops Publish docker images
 push-docker-images:
-	@echo $(DOCKER_PASSWORD) | docker login https://docker.pkg.github.com \
+	@echo $(DOCKER_PASSWORD) | docker login \
 		--username $(DOCKER_USERNAME) \
 		--password-stdin
 	for dir in $(SUBDIRS); do \
--- utils/meta.mk
+++ utils/meta.mk
@@ -17,7 +17,7 @@ IMAGE_NAME_WEBAPP=monitor-webapp
 IMAGE_NAME_HAPI=monitor-hapi
 IMAGE_NAME_WALLET=monitor-wallet
 
-DOCKER_REGISTRY=docker.pkg.github.com/eoscostarica/eosio-dashboard
+DOCKER_REGISTRY=eoscostarica506
 SUBDIRS = webapp hapi wallet
 
 MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME_WEBAPP IMAGE_NAME_HAPI IMAGE_NAME_WALLET
