6a3e7407ec71d516d57f8236d0ec3c0ee6e73803
Andres Gomez <kurono@riseup.net>
2020-12-31 15:10:50 -0600

    Create makefiles for the dashboard (#290)
    * feat: create makefile for building app. (refs #190)
    
    * feat: improve kubernetes makefile. refs #190.
    
    * feat: improve the webapp makefile. refs #190
    
    * feat: improve the hapi makefile. refs #190
    
    * feat: improve the wallet makefile. refs #190
    
    * feat: change the hasura URL depending on the environment. refs #190
    
    * feat: clean repo and add the makefile instrution to the build.
    refs #190.
    
    * feat: unify webapp deployment files and update deploy script. refs #190
    
    * feat: add configmap for the dashboard to the repo. refs #190
    
    * feat: create traefik ingress for the dashboard. refs #157
    
    * feat: Set environmental variables in GitHub Secrets and pass
    along in workflow yaml files. refs #159
    
    * fix cut line in lacchain.
    
    * feat: improve the env variables. refs #159.
    
    * feat: improve the env variables. refs #159.
    
    * fix some warnings.
    
    * add a github action for deplying 8s scripts. refs #159
    
    * feat: create stateful sets and longhorn backed volumes. refs #318
    
    * Use different namespaces for the dashboard environments.
    
    * improve makefile.
    
    * improve makefile.
    
    * use the docker.io registry until we solve the github registry error.
    refs #318.
    
    * improve kubernetes files.
    
    * improve wallet kubernetes deployment.
    
    * feat: improve wallet to create persistent volume claim. refs #318
    
    * feat: create ingress for webapp and hasura. refs #264
    
    * fix the graphql URL.
    
    * Feat: create a hasura container so we can include the migration
    and metadata directories. refs #319
    
    * Improve the makefiles. refs #319
    
    * clean the scripts directory which is not needed anymore.
    
    * Feat: Set environmental variables in GitHub Secrets and pass along
    in workflow yaml files. refs #159

--- utils/meta.mk
+++ utils/meta.mk
@@ -1,10 +1,27 @@
 -include .env
 
-VERSION=$(shell git rev-parse --short HEAD)
-IMAGE_NAME=""
-DOCKER_REGISTRY=""
+VERSION ?= $(shell git rev-parse --short HEAD)
+CURRENT_BRANCH ?= $(git rev-parse --abbrev-ref HEAD)
 
-MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME
+ifeq ($(CURRENT_BRANCH),master)
+	REACT_APP_HASURA_URL := https://graphql-mainnet.eosio.cr/v1/graphql
+else ifeq ($(CURRENT_BRANCH),dev)
+	REACT_APP_HASURA_URL := https://graphql-testnet.eosio.cr/v1/graphql
+else ifeq ($(CURRENT_BRANCH),lacchain)
+	REACT_APP_HASURA_URL := https://dashboard-graphql.latamlink.io/v1/graphql
+else
+	REACT_APP_HASURA_URL := http://localhost:8585/v1/graphql
+endif
+
+IMAGE_NAME_WEBAPP=monitor-webapp
+IMAGE_NAME_HAPI=monitor-hapi
+IMAGE_NAME_HASURA=monitor-hasura
+IMAGE_NAME_WALLET=monitor-wallet
+
+DOCKER_REGISTRY=eoscostarica506
+SUBDIRS = webapp hapi hasura wallet
+
+MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME_WEBAPP IMAGE_NAME_HAPI IMAGE_NAME_WALLET IMAGE_NAME_HASURA
 
 SHELL_EXPORT := $(foreach v,$(MAKE_ENV),$(v)='$($(v))')
 
