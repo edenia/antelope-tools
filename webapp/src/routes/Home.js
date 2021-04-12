15d3a7724ff678dc8fd8ad1065ffc2f389dc0ae0
adriel <adriexnet@gmail.com>
2021-04-12 17:41:03 -0600

    block transaction history (#370)
    * feat: add service to process block from state history plugin
    
    * feat: display transactions history on home route
    
    * feat: add translations for new keywords of transactions history
    
    * feat: improve perfonmance
    
    * fix: typo in translations file

--- webapp/src/routes/Home.js
+++ webapp/src/routes/Home.js
@@ -1,11 +1,12 @@
 /* eslint camelcase: 0 */
 import React, { lazy, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useLazyQuery } from '@apollo/react-hooks'
 import { useTranslation } from 'react-i18next'
+import moment from 'moment'
 
 import { formatWithThousandSeparator } from '../utils'
-import { NODES_QUERY } from '../gql'
+import { NODES_QUERY, BLOCK_TRANSACTIONS_HISTORY } from '../gql'
 
 const Card = lazy(() => import('@material-ui/core/Card'))
 const CardContent = lazy(() => import('@material-ui/core/CardContent'))
@@ -18,6 +19,18 @@ const TransactionsChart = lazy(() => import('../components/TransactionsChart'))
 const Home = () => {
   const dispatch = useDispatch()
   const { data: { loading, producers } = {} } = useQuery(NODES_QUERY)
+  const [
+    loadLastHourBlockTransactions,
+    lastHourBlockTransactions
+  ] = useLazyQuery(BLOCK_TRANSACTIONS_HISTORY)
+  const [loadLastDayBlockTransactions, lastDayBlockTransactions] = useLazyQuery(
+    BLOCK_TRANSACTIONS_HISTORY
+  )
+  const [
+    loadLastWeekBlockTransactions,
+    lastWeekBlockTransactions
+  ] = useLazyQuery(BLOCK_TRANSACTIONS_HISTORY)
+
   const info = useSelector((state) => state.eos.info)
   const tps = useSelector((state) => state.eos.tps)
   const tpb = useSelector((state) => state.eos.tpb)
@@ -67,10 +80,35 @@ const Home = () => {
     }
   }, [dispatch])
 
+  useEffect(() => {
+    loadLastHourBlockTransactions({
+      variables: {
+        start: moment().subtract(1, 'hour'),
+        end: moment()
+      }
+    })
+    loadLastDayBlockTransactions({
+      variables: {
+        start: moment().subtract(1, 'day'),
+        end: moment()
+      }
+    })
+    loadLastWeekBlockTransactions({
+      variables: {
+        start: moment().subtract(1, 'week'),
+        end: moment()
+      }
+    })
+  }, [
+    loadLastHourBlockTransactions,
+    loadLastDayBlockTransactions,
+    loadLastWeekBlockTransactions
+  ])
+
   return (
     <Grid item xs={12}>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={3}>
           <Card>
             <CardContent>
               <Typography component="p" variant="h6">
@@ -82,7 +120,7 @@ const Home = () => {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={3}>
           <Card>
             <CardContent>
               <Typography component="p" variant="h6">
@@ -94,7 +132,7 @@ const Home = () => {
             </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={3}>
           <Card>
             <CardContent>
               <Typography component="p" variant="h6">
@@ -103,6 +141,56 @@ const Home = () => {
               <Typography component="p" variant="h6">
                 {formatWithThousandSeparator(info.last_irreversible_block_num)}
               </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Card>
+            <CardContent>
+              <Typography component="p" variant="h6">
+                {t('transactionsHistory')}
+              </Typography>
+              <dl>
+                <dt>
+                  <Typography component="p" variant="subtitle1">
+                    {t('lastHour')}:
+                  </Typography>
+                </dt>
+                <dd>
+                  <Typography component="p" variant="subtitle2">
+                    {formatWithThousandSeparator(
+                      lastHourBlockTransactions?.data?.block?.aggregate?.sum
+                        ?.transactions_length || 0
+                    )}
+                  </Typography>
+                </dd>
+                <dt>
+                  <Typography component="p" variant="subtitle1">
+                    {t('lastDay')}:
+                  </Typography>
+                </dt>
+                <dd>
+                  <Typography component="p" variant="subtitle2">
+                    {formatWithThousandSeparator(
+                      lastDayBlockTransactions?.data?.block?.aggregate?.sum
+                        ?.transactions_length || 0
+                    )}
+                  </Typography>
+                </dd>
+                <dt>
+                  <Typography component="p" variant="subtitle1">
+                    {t('lastWeek')}:
+                  </Typography>
+                </dt>
+                <dd>
+                  <Typography component="p" variant="subtitle2">
+                    {formatWithThousandSeparator(
+                      lastWeekBlockTransactions?.data?.block?.aggregate?.sum
+                        ?.transactions_length || 0
+                    )}
+                  </Typography>
+                </dd>
+              </dl>
             </CardContent>
           </Card>
         </Grid>
