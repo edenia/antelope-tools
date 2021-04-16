e6a3ae93766e184ebc341b9e9fe308ea397c7c5f
Teto Gomez <terencio17@rocketmail.com>
2021-04-16 10:57:09 -0600

    Refactor transaction graphics (#404)
    * fix(FE): refactor transaction graphics
    
    * fix(FE): remove console log
    
    * fix(FE): code review changes

--- webapp/src/components/TransactionsChart.js
+++ webapp/src/components/TransactionsChart.js
@@ -11,8 +11,7 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.white,
     padding: theme.spacing(2),
     borderRadius: theme.spacing(1),
-    boxShadow:
-      '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)'
+    boxShadow: theme.shadows[8]
   },
   description: {
     fontWeight: 'normal'
