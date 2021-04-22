9f227ca76825e3aae8174acdc2b506856e98d067
Teto Gomez <terencio17@rocketmail.com>
2021-04-21 21:14:29 -0600

    fix(FE): fix breakpoints

--- webapp/src/routes/Home.js
+++ webapp/src/routes/Home.js
@@ -1,6 +1,7 @@
 /* eslint camelcase: 0 */
 import React, { lazy, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { makeStyles } from '@material-ui/styles'
 import { useQuery } from '@apollo/react-hooks'
 import { useTranslation } from 'react-i18next'
 
@@ -10,6 +11,7 @@ import { NODES_QUERY } from '../gql'
 const Card = lazy(() => import('@material-ui/core/Card'))
 const CardContent = lazy(() => import('@material-ui/core/CardContent'))
 const Grid = lazy(() => import('@material-ui/core/Grid'))
+const Box = lazy(() => import('@material-ui/core/Box'))
 const Typography = lazy(() => import('@material-ui/core/Typography'))
 const LinearProgress = lazy(() => import('@material-ui/core/LinearProgress'))
 const ProducersChart = lazy(() => import('../components/ProducersChart'))
@@ -20,8 +22,52 @@ const TransactionsLineChart = lazy(() =>
   import('../components/TransactionsLineChart')
 )
 
+const useStyles = makeStyles((theme) => ({
+  leftColumn: {
+    alignContent: 'space-between',
+    paddingRight: 0,
+    '& .MuiCard-root': {
+      height: 125,
+      marginBottom: theme.spacing(2),
+      '& .MuiTypography-body1': {
+        marginBottom: theme.spacing(2)
+      }
+    },
+    [theme.breakpoints.up('md')]: {
+      paddingRight: theme.spacing(2)
+    }
+  },
+  rightColumn: {
+    paddingLeft: 0,
+    marginBottom: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      paddingLeft: theme.spacing(2)
+    }
+  },
+  bottomRow: {
+    paddingTop: 0,
+    [theme.breakpoints.up('md')]: {
+      paddingTop: theme.spacing(1)
+    }
+  },
+  boxIrreversible: {
+    display: 'flex',
+    alignItems: 'baseline',
+    paddingTop: theme.spacing(3),
+    '& .MuiTypography-body1': {
+      marginBottom: '0 !important',
+      letterSpacing: '0.09px',
+      color: 'rgba(0, 0, 0, 0.54)',
+      '& strong': {
+        color: '#212121'
+      }
+    }
+  }
+}))
+
 const Home = () => {
   const dispatch = useDispatch()
+  const classes = useStyles()
   const { data: { loading, producers } = {} } = useQuery(NODES_QUERY)
   const info = useSelector((state) => state.eos.info)
   const tps = useSelector((state) => state.eos.tps)
@@ -94,16 +140,15 @@ const Home = () => {
   }, [dispatch])
 
   return (
-    <Grid item xs={12}>
-      <Grid container spacing={2} justify="space-between">
+    <Box>
+      <Grid container justify="space-between">
         <Grid
           container
           item
           xs={12}
           md={3}
-          style={{ alignContent: 'baseline' }}
+          className={classes.leftColumn}
           justify="flex-start"
-          spacing={2}
         >
           <Grid item xs={12}>
             <Card>
@@ -122,59 +167,23 @@ const Home = () => {
                 <Typography component="p" variant="h6">
                   {formatWithThousandSeparator(info.head_block_num)}
                 </Typography>
+                <Box className={classes.boxIrreversible}>
+                  <Typography>
+                    {`${t('lastBlock')}: `}
+                    <strong>
+                      {formatWithThousandSeparator(
+                        info.last_irreversible_block_num
+                      )}
+                    </strong>
+                  </Typography>
+                </Box>
               </CardContent>
             </Card>
           </Grid>
-
-          <Grid item xs={12}>
-            <Card>
-              <CardContent>
-                <Typography>{t('lastBlock')}</Typography>
-                <Typography component="p" variant="h6">
-                  {formatWithThousandSeparator(
-                    info.last_irreversible_block_num
-                  )}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Card>
-              <CardContent>
-                <Typography>
-                  {`${t('transactions')} ${t('lastHour')}`}
-                </Typography>
-                <Typography component="p" variant="h6">
-                  {formatWithThousandSeparator(
-                    lastHourBlockTransactions?.data?.block?.aggregate?.sum
-                      ?.transactions_length || 0
-                  )}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={12}>
-            <Card>
-              <CardContent>
-                <Typography>
-                  {`${t('transactions')} ${t('lastDay')}`}
-                </Typography>
-                <Typography component="p" variant="h6">
-                  {formatWithThousandSeparator(
-                    lastDayBlockTransactions?.data?.block?.aggregate?.sum
-                      ?.transactions_length || 0
-                  )}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={12}>
-            <TransactionsHistory t={t} />
-          </Grid>
+          <TransactionsHistory t={t} />
         </Grid>
         {loading && <LinearProgress />}
-        <Grid item xs={12} md={9}>
+        <Grid item xs={12} md={9} className={classes.rightColumn}>
           <Card>
             <CardContent>
               <Typography component="p" variant="h6">
@@ -188,7 +197,7 @@ const Home = () => {
           </Card>
         </Grid>
       </Grid>
-      <Grid container spacing={2}>
+      <Grid container className={classes.bottomRow}>
         <Grid item xs={12}>
           <Card>
             <CardContent>
@@ -200,7 +209,7 @@ const Home = () => {
           </Card>
         </Grid>
       </Grid>
-    </Grid>
+    </Box>
   )
 }
 
