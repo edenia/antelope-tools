657797c131702312e212eb319e6b8f671b0e31dc
Adriel Diaz <adriexnet@gmail.com>
2021-05-03 18:28:26 -0600

    feat(hapi): add service to save round and schedule history

--- hapi/src/services/index.js
+++ hapi/src/services/index.js
@@ -1,9 +1,11 @@
+const missedBlocksService = require('./missed-blocks.service')
 const producerService = require('./producer.service')
 const settingService = require('./setting.service')
 const stateHistoryPluginService = require('./state-history-plugin.service')
 const statsService = require('./stats.service')
 
 module.exports = {
+  missedBlocksService,
   producerService,
   settingService,
   stateHistoryPluginService,
