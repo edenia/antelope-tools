8956575089f001f49f7e25db12f53f643038073e
Adriel Diaz <adriexnet@gmail.com>
2021-05-03 18:13:10 -0600

    feat(hapi): add sequelize util

--- hapi/src/config/hasura.config.js
+++ hapi/src/config/hasura.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   url: process.env.HAPI_HASURA_URL,
-  adminSecret: process.env.HAPI_HASURA_ADMIN_SECRET
+  adminSecret: process.env.HAPI_HASURA_ADMIN_SECRET,
+  databaseUrl: process.env.HAPI_DATABASE_URL
 }
