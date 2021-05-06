cde78b8468fdfdb8af81300a68064acfe63d9c4e
Xavier Fernandez <xavier@crservers.com>
2021-05-06 00:39:37 -0600

    Resolve conflicts

--- hapi/src/services/index.js
+++ hapi/src/services/index.js
@@ -2,19 +2,12 @@ const missedBlocksService = require('./missed-blocks.service')
 const producerService = require('./producer.service')
 const settingService = require('./setting.service')
 const stateHistoryPluginService = require('./state-history-plugin.service')
-<<<<<<< HEAD
-=======
 const statsService = require('./stats.service')
->>>>>>> dev
 
 module.exports = {
   missedBlocksService,
   producerService,
   settingService,
-<<<<<<< HEAD
-  stateHistoryPluginService
-=======
   stateHistoryPluginService,
   statsService
->>>>>>> dev
 }
