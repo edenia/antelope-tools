147ad2795f267cd97190d814ac53ff06cedb43d4
Adriel Diaz <adriexnet@gmail.com>
2021-05-17 13:46:25 -0600

    feat(webapp): add util to get last 6 characters of transaction id and block explorer link

---
+++ webapp/src/utils/get-transaction-url.js
@@ -0,0 +1,15 @@
+import { eosConfig } from '../config'
+
+export const getTransactionUrl = (trx) => {
+  if (!trx || !eosConfig.blockExplorerUrl) {
+    return
+  }
+
+  return {
+    trxId: trx.substr(trx.length - 6, trx.length),
+    // eslint-disable-next-line
+    explorerUrl: eosConfig.blockExplorerUrl.replace('${transaction}', trx)
+  }
+}
+
+export default getTransactionUrl
