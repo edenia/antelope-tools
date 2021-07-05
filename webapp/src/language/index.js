e13e899b1d4e9fbf2674de7ff9c48c2a2fe3f4bd
Teto Gomez <terencio17@rocketmail.com>
2021-07-05 15:05:50 -0600

    fix(webapp): add support to new networks

--- webapp/src/language/index.js
+++ webapp/src/language/index.js
@@ -5,32 +5,44 @@ import en from './en.json'
 import es from './es.json'
 import enLacchain from './en.lacchain.json'
 import esLacchain from './es.lacchain.json'
-import enTelos from './en.telos-testnet.json'
-import esTelos from './es.telos-testnet.json'
+import enTelosTestnet from './en.telos-testnet.json'
+import esTelosTestnet from './es.telos-testnet.json'
+import enTelos from './en.telos.json'
+import esTelos from './es.telos.json'
 import enJungle from './en.jungle.json'
 import esJungle from './es.jungle.json'
 import enMainnet from './en.mainnet.json'
 import esMainnet from './es.mainnet.json'
-import enProton from './en.proton-testnet.json'
-import esProton from './es.proton-testnet.json'
-import enWax from './en.wax-testnet.json'
-import esWax from './es.wax-testnet.json'
+import enProtonTestnet from './en.proton-testnet.json'
+import esProtonTestnet from './es.proton-testnet.json'
+import enProton from './en.proton.json'
+import esProton from './es.proton.json'
+import enWaxTestnet from './en.wax-testnet.json'
+import esWaxTestnet from './es.wax-testnet.json'
+import enWax from './en.wax.json'
+import esWax from './es.wax.json'
 
 const languajes = {
   es,
   en,
   'es.lacchain': esLacchain,
   'en.lacchain': enLacchain,
-  'es.telos-testnet': esTelos,
-  'en.telos-testnet': enTelos,
+  'es.telos': esTelos,
+  'en.telos': enTelos,
+  'es.proton': esProton,
+  'en.proton': enProton,
+  'es.wax': esWax,
+  'en.wax': enWax,
   'es.jungle': esJungle,
   'en.jungle': enJungle,
   'es.mainnet': esMainnet,
   'en.mainnet': enMainnet,
-  'es.proton-testnet': esProton,
-  'en.proton-testnet': enProton,
-  'es.wax-testnet': esWax,
-  'en.wax-testnet': enWax
+  'es.telos-testnet': esTelosTestnet,
+  'en.telos-testnet': enTelosTestnet,
+  'es.proton-testnet': esProtonTestnet,
+  'en.proton-testnet': enProtonTestnet,
+  'es.wax-testnet': esWaxTestnet,
+  'en.wax-testnet': enWaxTestnet
 }
 
 const getLanguaje = (languaje) => {
