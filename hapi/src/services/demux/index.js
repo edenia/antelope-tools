e7cec4310370f0c3ffaa2f132a4d04f36848c131
Adriel Diaz <adriexnet@gmail.com>
2021-07-14 16:52:33 -0600

    fix(hapi): await for hasura before start demux watch

--- hapi/src/services/demux/index.js
+++ hapi/src/services/demux/index.js
@@ -1,12 +1,13 @@
 const { BaseActionWatcher } = require('demux')
 const { NodeosActionReader } = require('demux-eos')
 
-const { eosConfig } = require('../../config')
+const { eosConfig, hasuraConfig } = require('../../config')
+const { axiosUtil, sleepFor } = require('../utils')
 
 const ActionHandler = require('./action-handler')
 const handlerVersion = require('./handler-version')
 
-const init = () => {
+const init = async () => {
   const actionHandler = new ActionHandler([handlerVersion])
   const actionReader = new NodeosActionReader({
     onlyIrreversible: true,
@@ -15,6 +16,23 @@ const init = () => {
     nodeosEndpoint: eosConfig.apiEndpoint
   })
   const watcher = new BaseActionWatcher(actionReader, actionHandler, 250)
+  let hasuraReady = false
+
+  while (!hasuraReady) {
+    try {
+      await axiosUtil.instance.get(
+        hasuraConfig.url.replace('/v1/graphql', '/healthz')
+      )
+      hasuraReady = true
+    } catch (error) {
+      hasuraReady = false
+      console.log(
+        'waiting for hasura...',
+        hasuraConfig.url.replace('/v1/graphql', '/healthz')
+      )
+      await sleepFor(3)
+    }
+  }
 
   watcher.watch()
 }
