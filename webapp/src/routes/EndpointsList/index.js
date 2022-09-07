3ec3b9f235a20c4e9feb8835d1a757dab3121962
Torresmorah <torresmorah@gmail.com>
2022-09-07 12:54:18 -0600

    Refactor(EndpointsList): refactor components
    - Remove Grid and Box components
    - Split styles
    - Split endpoints table

--- webapp/src/routes/EndpointsList/index.js
+++ webapp/src/routes/EndpointsList/index.js
@@ -2,18 +2,11 @@
 import React, { useEffect, useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { useTranslation } from 'react-i18next'
+import { makeStyles } from '@mui/styles'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import LinearProgress from '@mui/material/LinearProgress'
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import Box from '@mui/material/Box'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
@@ -23,9 +16,15 @@ import moment from 'moment'
 
 import { PRODUCERS_QUERY } from '../../gql'
 
+import EndpointsTable from './EndpointsTable'
+import styles from './styles'
+
+const useStyles = makeStyles(styles)
+
 const limitOptions = [10, 20, 30, 50, 80, 130]
 
 const EndpointsList = () => {
+  const classes = useStyles()
   const { t } = useTranslation('endpointsListRoute')
   const [load, { loading, data }] = useLazyQuery(PRODUCERS_QUERY)
   const [producers, setProducers] = useState([])
@@ -33,35 +32,31 @@ const EndpointsList = () => {
   const [pagination, setPagination] = useState({
     page: 1,
     limit: 80,
-    totalPages: 0
+    totalPages: 0,
   })
 
   useEffect(() => {
     load({
       variables: {
         offset: (pagination.page - 1) * pagination.limit,
-        limit: pagination.limit
-      }
+        limit: pagination.limit,
+      },
     })
     // eslint-disable-next-line
   }, [pagination.page, pagination.limit])
 
   useEffect(() => {
-    if (!data?.info) {
-      return
-    }
+    if (!data?.info) return
 
     setPagination((prev) => ({
       ...prev,
-      totalPages: Math.ceil(data?.info.producers?.count / pagination.limit)
+      totalPages: Math.ceil(data?.info.producers?.count / pagination.limit),
     }))
     // eslint-disable-next-line
   }, [data?.info, pagination.limit])
 
   useEffect(() => {
-    if (!data?.producers) {
-      return
-    }
+    if (!data?.producers) return
 
     setProducers(
       data.producers
@@ -80,14 +75,12 @@ const EndpointsList = () => {
           endpoints: {
             api: producer.endpoints?.api || [],
             ssl: producer.endpoints?.ssl || [],
-            p2p: producer.endpoints?.p2p || []
-          }
-        }))
+            p2p: producer.endpoints?.p2p || [],
+          },
+        })),
     )
 
-    if (!data.producers?.[0]?.updated_at) {
-      return
-    }
+    if (!data.producers?.[0]?.updated_at) return
 
     setUpdatedAt(data.producers[0].updated_at)
     // eslint-disable-next-line
@@ -96,123 +89,64 @@ const EndpointsList = () => {
   const handleOnPageChange = (_, page) => {
     setPagination((prev) => ({
       ...prev,
-      page
+      page,
     }))
   }
 
   return (
-    <Grid item xs={12}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Card>
-            <CardContent>
-              <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-                p={1}
-              >
-                <Box>
-                  <Typography component="p" variant="h6">
-                    {t('title')} {t('producer')}
-                  </Typography>
-                  {updatedAt && (
-                    <Typography component="p" variant="caption">
-                      {t('updatedAt')}: {moment(updatedAt).format('LL')}
-                    </Typography>
-                  )}
-                </Box>
-                <FormControl variant="standard">
-                  <InputLabel id="selectLabel">{t('itemsPerPage')}</InputLabel>
-                  <Select
-                    style={{
-                      minWidth: 150
-                    }}
-                    labelId="selectLabel"
-                    value={pagination.limit || ''}
-                    onChange={(e) =>
-                      setPagination((prev) => ({
-                        ...prev,
-                        limit: parseInt(e.target.value)
-                      }))
-                    }
-                    fullWidth
-                  >
-                    {limitOptions.map((option) => (
-                      <MenuItem key={option} value={option}>
-                        {option}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </Box>
-              {loading && <LinearProgress />}
-              {!loading && producers.length > 0 && (
-                <TableContainer>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>{t('producer')}</TableCell>
-                        <TableCell>{t('api')}</TableCell>
-                        <TableCell>{t('ssl')}</TableCell>
-                        <TableCell>{t('p2p')}</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {producers.map((producer, index) => (
-                        <TableRow key={index}>
-                          <TableCell>{producer.name}</TableCell>
-                          <TableCell>
-                            {producer.endpoints.api.map((endpoint, index) => (
-                              <Typography key={index}>{endpoint}</Typography>
-                            ))}
-                            {!producer.endpoints.api.length && (
-                              <Typography>{t('N/A')}</Typography>
-                            )}
-                          </TableCell>
-                          <TableCell>
-                            {producer.endpoints.ssl.map((endpoint, index) => (
-                              <Typography key={index}>{endpoint}</Typography>
-                            ))}
-                            {!producer.endpoints.ssl.length && (
-                              <Typography>{t('N/A')}</Typography>
-                            )}
-                          </TableCell>
-                          <TableCell>
-                            {producer.endpoints.p2p.map((endpoint, index) => (
-                              <Typography key={index}>{endpoint}</Typography>
-                            ))}
-                            {!producer.endpoints.p2p.length && (
-                              <Typography>{t('N/A')}</Typography>
-                            )}
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              )}
-              {!loading && pagination.totalPages > 1 && (
-                <Box
-                  display="flex"
-                  justifyContent="center"
-                  alignItems="center"
-                  p={3}
-                >
-                  <Pagination
-                    count={pagination.totalPages}
-                    page={pagination.page}
-                    onChange={handleOnPageChange}
-                    variant="outlined"
-                    shape="rounded"
-                  />
-                </Box>
-              )}
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </Grid>
+    <Card>
+      <CardContent>
+        <div className={classes.titleContainer}>
+          <div className={classes.dateContainer}>
+            <Typography component="p" variant="h6">
+              {t('title')} {t('producer')}
+            </Typography>
+            {updatedAt && (
+              <Typography component="p" variant="caption">
+                {t('updatedAt')}: {moment(updatedAt).format('LL')}
+              </Typography>
+            )}
+          </div>
+          <FormControl variant="standard">
+            <InputLabel id="selectLabel">{t('itemsPerPage')}</InputLabel>
+            <Select
+              className={classes.select}
+              labelId="selectLabel"
+              value={pagination.limit || ''}
+              onChange={(e) =>
+                setPagination((prev) => ({
+                  ...prev,
+                  limit: parseInt(e.target.value),
+                }))
+              }
+              fullWidth
+            >
+              {limitOptions.map((option) => (
+                <MenuItem key={option} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+        {loading ? (
+          <LinearProgress />
+        ) : (
+          !!producers.length && <EndpointsTable producers={producers} />
+        )}
+        {!loading && pagination.totalPages > 1 && (
+          <div className={classes.pagination}>
+            <Pagination
+              count={pagination.totalPages}
+              page={pagination.page}
+              onChange={handleOnPageChange}
+              variant="outlined"
+              shape="rounded"
+            />
+          </div>
+        )}
+      </CardContent>
+    </Card>
   )
 }
 
---
+++ webapp/src/routes/EndpointsList/styles.js
@@ -0,0 +1,17 @@
+export default (theme) => ({
+  titleContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  dateContainer: {
+    flex: '70%',
+  },
+  select: {
+    minWidth: 150,
+  },
+  pagination: {
+    display: 'flex',
+    padding: theme.spacing(4),
+    justifyContent: 'center',
+  },
+})
