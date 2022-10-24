b803d1b9d80fea52f4f871255246457a5be28e0b
Fabian Vives <f.vives99@gmail.com>
2022-10-24 16:15:12 -0600

    fix(webapp): bpjson style

--- webapp/src/routes/BPJson/styles.js
+++ webapp/src/routes/BPJson/styles.js
@@ -1,5 +1,5 @@
 export default (theme) => ({
-    cardShadow: {
-      boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.15) !important',
-    },
-  })
+  cardShadow: {
+    boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.15) !important',
+  },
+})
