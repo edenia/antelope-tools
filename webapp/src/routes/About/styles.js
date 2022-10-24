7e4657e102f0e3ff9922c94db30f3d1be6c67848
Fabian Vives <f.vives99@gmail.com>
2022-10-24 16:38:43 -0600

    chore(webapp): about style + removed box component

--- webapp/src/routes/About/styles.js
+++ webapp/src/routes/About/styles.js
@@ -3,21 +3,31 @@ export default (theme) => ({
     '& p': {
       fontSize: 14,
       lineHeight: '21px',
-      letterSpacing: '0.07875px'
+      letterSpacing: '0.07875px',
     },
     '& h4': {
       fontWeight: '600',
       fontSize: 20,
       lineHeight: '24px',
       textAlign: 'justify',
-      marginBottom: theme.spacing(4)
-    }
+      marginBottom: theme.spacing(4),
+    },
   },
   logo: {
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 569,
-      height: 498
-    }
+    width: 569,
+    height: 498,
+    [theme.breakpoints.down('md')]: {
+      width: '85%',
+    },
+  },
+  cardShadow: {
+    boxShadow: '0px 1px 5px rgba(0, 0, 0, 0.15) !important',
+  },
+  mainText:{
+    display: 'flex',
+    flexDirection: 'row',
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column'
+    },
   }
 })
