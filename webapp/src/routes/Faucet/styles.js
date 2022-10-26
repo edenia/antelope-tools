bc83bab630077f028e44daff337d29d5c4281110
Fabian Vives <f.vives99@gmail.com>
2022-10-26 14:52:48 -0600

    refactor(webapp): faucet box component + styles

--- webapp/src/routes/Faucet/styles.js
+++ webapp/src/routes/Faucet/styles.js
@@ -1,5 +1,18 @@
 export default (theme) => ({
   formControl: {
-    margin: theme.spacing(4, 4, 0, 0)
-  }
+    margin: theme.spacing(2),
+  },
+  test: {
+    display: 'flex',
+
+    [theme.breakpoints.down('md')]: {
+      display: 'block',
+    },
+  },
+  card: {
+    padding: '10px',
+  },
+  button: {
+    marginTop: '16px',
+  },
 })
