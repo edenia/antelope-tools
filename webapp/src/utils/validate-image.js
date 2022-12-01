9ad336b5eac33b4461c6dfef18619d75dd3305bf
Torresmorah <torresmorah@gmail.com>
2022-12-01 11:25:04 -0600

    feat(webapp): follow best practices for images
    - load only images with https
    - check if the images are square
    - increase the size of default network logos to keep the aspect ratio

--- webapp/src/utils/validate-image.js
+++ webapp/src/utils/validate-image.js
@@ -1,9 +1,11 @@
 const isLogoValid = (imgUrl) => {
+  if (!imgUrl || !imgUrl.startsWith('https')) return false
+
   const img = new Image()
 
   img.src = imgUrl
 
-  return img.width >= 100 && img.height >= 100
+  return img.width >= 100 && img.height === img.width
 }
 
 export default isLogoValid
