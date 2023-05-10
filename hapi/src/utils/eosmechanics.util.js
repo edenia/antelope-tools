c448f686a19086ec6e708c76dad7b44469a362f6
Torresmorah <torresmorah@gmail.com>
2023-05-10 14:16:35 -0600

    feat(hapi): add custum permission for cpu benchmark

--- hapi/src/utils/eosmechanics.util.js
+++ hapi/src/utils/eosmechanics.util.js
@@ -2,30 +2,18 @@ const { eosConfig } = require('../config')
 
 const eosUtil = require('./eos.util')
 
-const cpu = async () => {
-  const actions = []
-
+const transact = async actions => {
   if (eosConfig.eosmechanics.includeTransaction) {
     actions.push(eosConfig.eosmechanics.includeTransaction)
   }
-
-  actions.push({
-    authorization: [
-      {
-        actor: eosConfig.eosmechanics.account,
-        permission: 'active'
-      }
-    ],
-    account: eosConfig.eosmechanics.account,
-    name: 'cpu',
-    data: {}
-  })
 
   const transaction = await eosUtil.transact(
     actions,
     eosConfig.eosmechanics.account,
     eosConfig.eosmechanics.password
   )
+
+  await new Promise((resolve) => setTimeout(() => resolve(), 1000))
   const block = await eosUtil.getBlock(transaction.processed.block_num)
 
   return {
@@ -34,18 +22,32 @@ const cpu = async () => {
   }
 }
 
-const net = async (input = '') => {
+const cpu = async () => {
   const actions = []
-
-  if (eosConfig.eosmechanics.includeTransaction) {
-    actions.push(eosConfig.eosmechanics.includeTransaction)
-  }
 
   actions.push({
     authorization: [
       {
         actor: eosConfig.eosmechanics.account,
-        permission: 'active'
+        permission: eosConfig.eosmechanics.customPermission
+      }
+    ],
+    account: eosConfig.eosmechanics.account,
+    name: 'cpu',
+    data: {}
+  })
+
+  return await transact(actions)
+}
+
+const net = async (input = '') => {
+  const actions = []
+
+  actions.push({
+    authorization: [
+      {
+        actor: eosConfig.eosmechanics.account,
+        permission: eosConfig.eosmechanics.customPermission
       }
     ],
     account: eosConfig.eosmechanics.account,
@@ -55,32 +57,17 @@ const net = async (input = '') => {
     }
   })
 
-  const transaction = await eosUtil.transact(
-    actions,
-    eosConfig.eosmechanics.account,
-    eosConfig.eosmechanics.password
-  )
-  await new Promise(resolve => setTimeout(() => resolve(), 500))
-  const block = await eosUtil.getBlock(transaction.processed.block_num)
-
-  return {
-    transaction,
-    block
-  }
+  return await transact(actions)
 }
 
 const ram = async () => {
   const actions = []
 
-  if (eosConfig.eosmechanics.includeTransaction) {
-    actions.push(eosConfig.eosmechanics.includeTransaction)
-  }
-
   actions.push({
     authorization: [
       {
         actor: eosConfig.eosmechanics.account,
-        permission: 'active'
+        permission: eosConfig.eosmechanics.customPermission
       }
     ],
     account: eosConfig.eosmechanics.account,
@@ -88,18 +75,7 @@ const ram = async () => {
     data: {}
   })
 
-  const transaction = await eosUtil.transact(
-    actions,
-    eosConfig.eosmechanics.account,
-    eosConfig.eosmechanics.password
-  )
-  await new Promise(resolve => setTimeout(() => resolve(), 500))
-  const block = await eosUtil.getBlock(transaction.processed.block_num)
-
-  return {
-    transaction,
-    block
-  }
+  return await transact(actions)
 }
 
 module.exports = {
