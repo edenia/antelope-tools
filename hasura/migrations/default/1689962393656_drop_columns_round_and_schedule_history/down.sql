7aeec806e6b15867479b20388462614ed1af89af
Torresmorah <torresmorah@gmail.com>
2023-07-21 13:10:03 -0600

    chore(hasura): remove unused fields in the round_history and schedule_history tables

---
+++ hasura/migrations/default/1689962393656_drop_columns_round_and_schedule_history/down.sql
@@ -0,0 +1,12 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- alter table "public"."round_history"
+-- drop column "updated_at",
+-- drop column "started_at",
+-- drop column "produced_blocks";
+--
+-- alter table "public"."schedule_history"
+-- drop column "updated_at",
+-- drop column "current",
+-- drop column "last_block",
+-- drop column "last_block_at";
