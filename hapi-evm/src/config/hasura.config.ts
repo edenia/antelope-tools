c0c9ac703c7380a35d348580ece001b1c4b14621
Torresmorah <torresmorah@gmail.com>
2023-08-11 10:46:01 -0600

    chore(hapi-evm): use Hasura env variables

--- hapi-evm/src/config/hasura.config.ts
+++ hapi-evm/src/config/hasura.config.ts
@@ -1,8 +1,6 @@
-export const url =
-  process.env.HAPI_HASURA_URL || 'http://hasura:8080/v1/graphql'
-export const adminSecret =
-  process.env.HAPI_HASURA_ADMIN_SECRET || 'myadminsecretkey'
-export const databaseURL = process.env.HAPI_DATABASE_URL
+export const url = process.env.HAPI_EVM_HASURA_URL || ''
+export const adminSecret = process.env.HAPI_EVM_HASURA_ADMIN_SECRET || ''
+export const databaseURL = process.env.HAPI_EVM_DATABASE_URL || ''
 
 if (!url || !adminSecret || !databaseURL) {
   throw new Error('Missing required hasura env variables')
