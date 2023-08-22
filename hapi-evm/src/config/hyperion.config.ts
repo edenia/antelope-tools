78ed7bf2fb6dbbe721cb52ed4e75d1422a5f8cc4
Leister Francisco Alvarado Campos <leisterac.1997@gmail.com>
2023-08-22 10:18:54 -0600

    feat(cicd): update workflow for specific networks with evm

--- hapi-evm/src/config/hyperion.config.ts
+++ hapi-evm/src/config/hyperion.config.ts
@@ -1,4 +1,4 @@
 export const api =
-  process.env.HAPI_HYPERION_API || 'https://test.telos.eosusa.io'
+  process.env.HAPI_EVM_HYPERION_API || 'https://test.telos.eosusa.io'
 export const startAt =
-  process.env.HAPI_HYPERION_START_AT || '2021-06-02T00:00:00.000+00:00'
+  process.env.HAPI_EVM_HYPERION_START_AT || '2021-06-02T00:00:00.000+00:00'
