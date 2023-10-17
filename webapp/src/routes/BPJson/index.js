6de7160d183eebae5cf150d74d95bd24c503748a
Torresmorah <torresmorah@gmail.com>
2023-10-17 16:45:47 -0600

    chore(webapp): remove styles

--- webapp/src/routes/BPJson/index.js
+++ webapp/src/routes/BPJson/index.js
@@ -3,17 +3,12 @@ import LinearProgress from '@mui/material/LinearProgress'
 import Typography from '@mui/material/Typography'
 import Alert from '@mui/material/Alert'
 import { BPJsonGenerator } from '@eoscostarica/eoscr-components'
-import { makeStyles } from '@mui/styles'
 
 import { eosConfig } from '../../config'
 
 import useBPJsonState from './useBPJsonState'
-import styles from './styles'
-
-const useStyles = makeStyles(styles)
 
 const BPJson = () => {
-  const classes = useStyles()
   const [
     { loading, inconsistencyMessage, initData, producer, error, ual },
     { t, handleOnSubmit },
