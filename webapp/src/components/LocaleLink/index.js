2ce159bb89251415f47adbcf3b748e472c98c81e
Torresmorah <torresmorah@gmail.com>
2023-11-14 16:09:18 -0600

    fix(webapp): fix hold ref in LocaleLink component

--- webapp/src/components/LocaleLink/index.js
+++ webapp/src/components/LocaleLink/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
 import { getLocalePath } from '../../utils/url-localization'
 
-const LocaleLink = props => {
+const LocaleLink = forwardRef((props, ref) => {
   const { i18n } = useTranslation()
 
-  return <Link {...props} to={getLocalePath(props.to,props.locale || i18n.language)}/>
-}
+  return <Link {...props} to={getLocalePath(props.to,props.locale || i18n.language)} ref={ref}/>
+})
 
 export default LocaleLink
